ecfdc8408aa00f2af243a03a345f7471cfa6059d
etuong <etuong@gmail.com>
2022-07-15 14:39:26 -0700

    Updated readme with screenshot and formatted files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Oracle has a layout manager called TableLayout which is an alternative for GridBagLayout for Java Swing.
+# Swing Table Layout Utility
+
+![alt text](screenshot.png)
+
+Oracle has a layout manager called TableLayout which is an alternative for GridBagLayout for Java Swing. Essentially, to create a layout in Swing, you need to initialize the panel with appropriate and respective dimensions. This can be a major hassle especially if the layout is complex or tedious.
 
 This simple and straightforward web-based utility helper gives the users the capability to "draw" their table and generate the strings for the TableLayout that they can copy and paste to their program. It also support cell merging and save/load via JSON. The textform on each cell is to designate a placeholder for the actual JComponent in Swing.
--- script.js
+++ script.js
@@ -20,14 +20,14 @@ deleteButton.style.display = "none";
 document.getElementById('file-input').style.display = "none";
 
 const tableLocation = {
-    COLUMN: 'column',
-    ROW: 'row',
-    CELL: 'cell',
+	COLUMN: 'column',
+	ROW: 'row',
+	CELL: 'cell',
 }
 
-function onLoadMethod() {  
+function onLoadMethod() {
 	// Initially load four rows and three columns  
-    createTable(undefined, 5, 4);
+	createTable(undefined, 5, 4);
 }
 
 function createTable(data, rowLength, columnLength) {
@@ -47,7 +47,7 @@ function createTable(data, rowLength, columnLength) {
 				if (data === undefined) {
 					var x = row.insertCell(c);
 					x.innerHTML = returnTemplate(tableLocation.CELL, "");
-				} else if (typeof data[r][c] !== 'undefined'){
+				} else if (typeof data[r][c] !== 'undefined') {
 					var x = row.insertCell(c);
 					x.rowSpan = data[r][c].rowSpan;
 					x.colSpan = data[r][c].colSpan;
@@ -60,8 +60,8 @@ function createTable(data, rowLength, columnLength) {
 
 function returnTemplate(selectedLocation, value) {
 	// Row, column, and cell template
-	
-	switch(selectedLocation){
+
+	switch (selectedLocation) {
 		case tableLocation.COLUMN:
 			return "<button class='btn btn-block btn-row' onClick='selectColumn(this)'>Column " + value + " </button>";
 		case tableLocation.ROW:
@@ -73,143 +73,143 @@ function returnTemplate(selectedLocation, value) {
 
 function selectRow(e) {
 	rowLength = table.rows.length;
-    columnLength = table.rows[0].cells.length;
-    selectedRow = e.parentNode.parentNode;
+	columnLength = table.rows[0].cells.length;
+	selectedRow = e.parentNode.parentNode;
 
 	// Clear any selected column
 	for (i = 1; i < rowLength; i++) {
 		if (toggleColumn != null)
 			table.rows[i].cells[toggleColumn].classList.remove("selectedElement");
 	}
-	
+
 	if (toggleRow == selectedRow) { // Row is reselected, deselect row
 		for (c = 1; c < columnLength; c++) {
-            selectedRow.cells[c].classList.remove("selectedElement");
-        }
-        toggleRow = null;
-        selectedRow = 0;
-    } else { // A different row is selected
-        for (c = 1; c < columnLength; c++) {			
-            selectedRow.cells[c].className = 'selectedElement';
+			selectedRow.cells[c].classList.remove("selectedElement");
+		}
+		toggleRow = null;
+		selectedRow = 0;
+	} else { // A different row is selected
+		for (c = 1; c < columnLength; c++) {
+			selectedRow.cells[c].className = 'selectedElement';
 			if (toggleRow != null)
 				toggleRow.cells[c].classList.remove("selectedElement");
-        }
-        toggleRow = selectedRow;
-    }
+		}
+		toggleRow = selectedRow;
+	}
 
-    // Display the add and delete buttons
-    if (selectedRow == 0) {
-        hideAddDeleteButtons();
-    } else {
-        addButton.style.display = "inline-block";
+	// Display the add and delete buttons
+	if (selectedRow == 0) {
+		hideAddDeleteButtons();
+	} else {
+		addButton.style.display = "inline-block";
 		addButton.innerHTML = "Add Row";
-        deleteButton.style.display = "inline-block";
+		deleteButton.style.display = "inline-block";
 		deleteButton.innerHTML = "Delete Row";
-    }
+	}
 
-    selectedColumn = 0;
-    toggleColumn = null;
+	selectedColumn = 0;
+	toggleColumn = null;
 }
 
 function selectColumn(e) {
-    rowLength = table.rows.length;
+	rowLength = table.rows.length;
 	columnLength = table.rows[0].cells.length;
-    selectedColumn = e.parentNode;
-    selectedColumnIndex = selectedColumn.cellIndex;
+	selectedColumn = e.parentNode;
+	selectedColumnIndex = selectedColumn.cellIndex;
 
 	// Clear any selected row
-	for (c = 1; c < columnLength; c++) {			
+	for (c = 1; c < columnLength; c++) {
 		if (toggleRow != null)
 			toggleRow.cells[c].classList.remove("selectedElement");
 	}
-	
+
 	if (toggleColumn == selectedColumnIndex) {
-        for (i = 1; i < rowLength; i++) {
-            table.rows[i].cells[selectedColumnIndex].classList.remove("selectedElement");
-        }
-        toggleColumn = null;
-        selectedColumn = 0;
-    } else {
-        for (i = 1; i < rowLength; i++) {
-            table.rows[i].cells[selectedColumnIndex].className = 'selectedElement';
+		for (i = 1; i < rowLength; i++) {
+			table.rows[i].cells[selectedColumnIndex].classList.remove("selectedElement");
+		}
+		toggleColumn = null;
+		selectedColumn = 0;
+	} else {
+		for (i = 1; i < rowLength; i++) {
+			table.rows[i].cells[selectedColumnIndex].className = 'selectedElement';
 			if (toggleColumn != null)
 				table.rows[i].cells[toggleColumn].classList.remove("selectedElement");
-        }
-        toggleColumn = selectedColumnIndex;
-    }
+		}
+		toggleColumn = selectedColumnIndex;
+	}
 
 
-    if (selectedColumn == 0) {
-        hideAddDeleteButtons();
-    } else {
-        addButton.style.display = "inline-block";
+	if (selectedColumn == 0) {
+		hideAddDeleteButtons();
+	} else {
+		addButton.style.display = "inline-block";
 		addButton.innerHTML = "Add Column";
-        deleteButton.style.display = "inline-block";
+		deleteButton.style.display = "inline-block";
 		deleteButton.innerHTML = "Delete Column";
-    }
+	}
 
-    selectedRow = 0;
-    toggleRow = null;
+	selectedRow = 0;
+	toggleRow = null;
 }
 
 function onAdd() {
 
-    columnLength = table.rows[0].cells.length;
-    rowLength = table.rows.length;
-	
-    if (selectedRow != 0) {
+	columnLength = table.rows[0].cells.length;
+	rowLength = table.rows.length;
 
-        var row = table.insertRow(selectedRow.rowIndex + 1);
+	if (selectedRow != 0) {
 
-        for (c = 0; c < columnLength; c++) {
-            var cell = row.insertCell(c);
+		var row = table.insertRow(selectedRow.rowIndex + 1);
 
-            if (c == 0) {
-                cell.innerHTML = returnTemplate(tableLocation.ROW, selectedRow.rowIndex + 1);
-            } else {
-                cell.innerHTML = returnTemplate(tableLocation.CELL, "");
-            }
-            selectedRow.cells[c].classList.remove("selectedElement");
-        }
-    } else if (selectedColumn != 0) {
+		for (c = 0; c < columnLength; c++) {
+			var cell = row.insertCell(c);
 
-        for (i = 0; i < rowLength; i++) {
-            if (i == 0) {
-                table.rows[i].insertCell(selectedColumnIndex + 1).innerHTML = returnTemplate(tableLocation.COLUMN, selectedColumnIndex.rowIndex + 1);
-            } else {
-                table.rows[i].insertCell(selectedColumnIndex + 1).innerHTML = returnTemplate(tableLocation.CELL, "");
+			if (c == 0) {
+				cell.innerHTML = returnTemplate(tableLocation.ROW, selectedRow.rowIndex + 1);
+			} else {
+				cell.innerHTML = returnTemplate(tableLocation.CELL, "");
+			}
+			selectedRow.cells[c].classList.remove("selectedElement");
+		}
+	} else if (selectedColumn != 0) {
 
-            }
-            table.rows[i].cells[selectedColumnIndex].classList.remove("selectedElement");
-        }
-    } else {
-        console.log("Error in Adding Column or Row");
-    }
+		for (i = 0; i < rowLength; i++) {
+			if (i == 0) {
+				table.rows[i].insertCell(selectedColumnIndex + 1).innerHTML = returnTemplate(tableLocation.COLUMN, selectedColumnIndex.rowIndex + 1);
+			} else {
+				table.rows[i].insertCell(selectedColumnIndex + 1).innerHTML = returnTemplate(tableLocation.CELL, "");
+
+			}
+			table.rows[i].cells[selectedColumnIndex].classList.remove("selectedElement");
+		}
+	} else {
+		console.log("Error in Adding Column or Row");
+	}
 
 	refreshTable();
 }
 
 function onDelete() {
 
-    columnLength = table.rows[0].cells.length;
-    rowLength = table.rows.length;	
-	
-    if (selectedRow != 0 && rowLength > 2) {
-        table.deleteRow(selectedRow.rowIndex);
-    } else if (selectedColumn != 0 && columnLength > 2) {
+	columnLength = table.rows[0].cells.length;
+	rowLength = table.rows.length;
 
-        for (i = 0; i < rowLength; i++) {
-            table.rows[i].deleteCell(selectedColumnIndex);
-        }
-    } else {
-        for (i = 0; i < rowLength; i++) {
-            for (j = 0; j < columnLength; j++) {
-                table.rows[i].cells[j].classList.remove("selectedElement");
-            }
-        }
-        console.log("Error in Deleting Row or Column");
-        alert("1 Row or Column required");
-    }
+	if (selectedRow != 0 && rowLength > 2) {
+		table.deleteRow(selectedRow.rowIndex);
+	} else if (selectedColumn != 0 && columnLength > 2) {
+
+		for (i = 0; i < rowLength; i++) {
+			table.rows[i].deleteCell(selectedColumnIndex);
+		}
+	} else {
+		for (i = 0; i < rowLength; i++) {
+			for (j = 0; j < columnLength; j++) {
+				table.rows[i].cells[j].classList.remove("selectedElement");
+			}
+		}
+		console.log("Error in Deleting Row or Column");
+		alert("1 Row or Column required");
+	}
 
 	refreshTable();
 }
@@ -217,110 +217,108 @@ function onDelete() {
 function onCancel() {
 	isMerging = false;
 	document.getElementById('mergeText').innerHTML = "";
-	
+
 	var buttons = document.getElementsByTagName("button");
-	for(var i = 0; i < buttons.length; i++) 
-	{
+	for (var i = 0; i < buttons.length; i++) {
 		buttons[i].disabled = false;
 		buttons[i].classList.remove("mergingBackground");
 	}
 	document.getElementById('cancelButton').disabled = true;
-	
+
 	firstSelected = null;
 	secondSelected = null;
 }
 
-function onMerge(){
-	
+function onMerge() {
+
 	isMerging = true;
 	document.getElementById('mergeText').innerHTML = "Please select the first point";
 
 	var buttons = document.getElementsByTagName("button");
-	for(var i = 0; i < buttons.length; i++) 
-	{
+	for (var i = 0; i < buttons.length; i++) {
 		if (buttons[i].innerHTML != "Cancel") {
 			buttons[i].disabled = true;
 			buttons[i].classList.add("mergingBackground");
 		}
 	}
-	document.getElementById('cancelButton').disabled = false;	
+	document.getElementById('cancelButton').disabled = false;
 }
 
-var coordinate = function findCellIndices(cell){
+var coordinate = function findCellIndices(cell) {
 	rowLength = table.rows.length;
 	columnLength = table.rows[0].cells.length;
 	for (r = 0; r < rowLength; r++) {
 		for (c = 0; c < columnLength; c++) {
 			if (table.rows[r].cells[c] === cell)
-				return [r,c];
+				return [r, c];
 		}
 	}
 }
 
-function mergeFinished(){
-	
+function mergeFinished() {
+
 	// Get coordinates
 	var coordinate1 = new coordinate(firstSelected);
 	var coordinate2 = new coordinate(secondSelected);
-	
+
 	if (coordinate1[0] == coordinate2[0] && coordinate1[1] == coordinate2[1]) {
 		alert("You can't merge the same cell!");
-	} else {		
+	} else {
 		var rowIndex = Math.min(coordinate1[0], coordinate2[0]);
 		var rowSpan = Math.abs(coordinate1[0] - coordinate2[0]);
-		
+
 		var columnIndex = Math.min(coordinate1[1], coordinate2[1]);
 		var columnSpan = Math.abs(coordinate1[1] - coordinate2[1]);
-		
+
 		// For vertical merging
 		if (columnSpan == 0) {
 			for (var i = rowIndex + 1; i <= rowIndex + rowSpan; i++) {
-				table.rows[i].deleteCell(columnIndex);				
+				table.rows[i].deleteCell(columnIndex);
 			}
 		}
 		// For horizontal merging
 		else if (rowSpan == 0) {
-			for (var j = columnIndex+1; j <= columnIndex + columnSpan; j++) {
-				table.rows[rowIndex].deleteCell(1);				
+			for (var j = columnIndex + 1; j <= columnIndex + columnSpan; j++) {
+				table.rows[rowIndex].deleteCell(1);
 			}
 		}
 		// For vertical and horizontal merging
 		else {
-			for (var i = rowIndex; i <= rowIndex + rowSpan; i++) {				
+			for (var i = rowIndex; i <= rowIndex + rowSpan; i++) {
 				for (var j = columnIndex; j <= columnIndex + columnSpan; j++) {
 					if (!(i == rowIndex && j == columnIndex))
-						table.rows[i].deleteCell(1);	
-				}				
-			}			
+						table.rows[i].deleteCell(1);
+				}
+			}
 		}
-		
+
 		if (++columnSpan > 1)
 			table.rows[rowIndex].cells[columnIndex].colSpan = columnSpan;
 		if (++rowSpan > 1)
 			table.rows[rowIndex].cells[columnIndex].rowSpan = rowSpan;
 	}
-	onCancel();	
+	onCancel();
 }
 
-function mergeInProcess(cell){
-	if(isMerging){
-		if(firstSelected == null){		
+function mergeInProcess(cell) {
+	if (isMerging) {
+		if (firstSelected == null) {
 			firstSelected = cell;
 			document.getElementById('mergeText').innerHTML = "Please select second point";
-		} else if(secondSelected == null){
+		} else if (secondSelected == null) {
 			secondSelected = cell;
 			mergeFinished();
-		} 	
+		}
 	}
 }
 
 function refreshTable() {
 	hideAddDeleteButtons();
-    tableNumbering();
-    selectedColumn = 0;
-    selectedRow = 0;
-    toggleRow = null;
-    toggleColumn = null;	
+	tableNumbering();
+	selectedColumn = 0;
+	selectedRow = 0;
+	toggleRow = null;
+	toggleColumn = null;
 }
 
 function hideAddDeleteButtons() {
@@ -331,20 +329,20 @@ function hideAddDeleteButtons() {
 function onOutput() {
 	var html = [];
 	html.push("JPanel panel = new JPanel(new TableLayout(\n\tnew double[] {\n");
-	
+
 	columnLength = table.rows[0].cells.length;
-    rowLength = table.rows.length;
-	
+	rowLength = table.rows.length;
+
 	for (var i = 1; i < columnLength; i++) {
 		html.push("\t\tSPACE, TableLayout.PREFERRED, // Column " + i + "\n");
-	}    
+	}
 	html.push("\t\tSPACE },\n\tnew double[] {\n");
-	
+
 	for (var i = 1; i < rowLength; i++) {
 		html.push("\t\tSPACE, TableLayout.PREFERRED, // Row " + i + "\n");
 	}
-	html.push("\t\tSPACE}));\n\n");	
-	
+	html.push("\t\tSPACE}));\n\n");
+
 	for (r = 1; r < rowLength; r++) {
 		for (c = 1; c < columnLength; c++) {
 			var td = table.rows[r].cells[c];
@@ -362,19 +360,19 @@ function onOutput() {
 			}
 		}
 	}
-	
-    var outputArea = document.getElementById("output");
+
+	var outputArea = document.getElementById("output");
 	outputArea.value = html.join("");
 }
 
 function tableNumbering() {
-    columnLength = table.rows[0].cells.length;
-    rowLength = table.rows.length;
+	columnLength = table.rows[0].cells.length;
+	rowLength = table.rows.length;
 
 	for (r = 1; r < rowLength; r++) {
-		table.rows[r].cells[0].firstChild.innerHTML = "Row " + r;  
-    }
-	
+		table.rows[r].cells[0].firstChild.innerHTML = "Row " + r;
+	}
+
 	for (c = 1; c < columnLength; c++) {
 		table.rows[0].cells[c].firstChild.innerHTML = "Column " + c;
 	}
@@ -382,34 +380,34 @@ function tableNumbering() {
 
 function onSave() {
 
-    var columnLength = table.rows[0].cells.length - 1;
-    var rowLength = table.rows.length - 1;
+	var columnLength = table.rows[0].cells.length - 1;
+	var rowLength = table.rows.length - 1;
 	var dataArray = new Array();
-	
-	dataArray[0] = {rowLength, columnLength};
-	
-    for (var i = 1; i <= rowLength; i++) {
 
-        dataArray[i] = new Array();
+	dataArray[0] = { rowLength, columnLength };
 
-        for (var j = 1; j <= columnLength; j++) {
+	for (var i = 1; i <= rowLength; i++) {
+
+		dataArray[i] = new Array();
+
+		for (var j = 1; j <= columnLength; j++) {
 			var cell = table.rows[i].cells[j];
 			if (typeof cell != 'undefined') {
-				dataArray[i][j] = {value:cell.children[0].value, rowSpan:cell.rowSpan, colSpan:cell.colSpan};
+				dataArray[i][j] = { value: cell.children[0].value, rowSpan: cell.rowSpan, colSpan: cell.colSpan };
 			}
-        }
-    }
-	
+		}
+	}
+
 	console.log(dataArray);
-    var jsonData = JSON.stringify(dataArray);	
-    var blob = new Blob([jsonData], {type: "text/plain;charset=utf-8"});
+	var jsonData = JSON.stringify(dataArray);
+	var blob = new Blob([jsonData], { type: "text/plain;charset=utf-8" });
 	saveAs(blob, "tableLayoutHelper.json");
 }
 
 function onLoad() {
 	var reader = new FileReader();
-	
-	reader.onload = function(event) {
+
+	reader.onload = function (event) {
 		var jsonObj = JSON.parse(event.target.result);
 		console.log(jsonObj);
 		var columnLength = jsonObj[0].columnLength + 1;
@@ -417,6 +415,6 @@ function onLoad() {
 		createTable(jsonObj, rowLength, columnLength);
 	}
 
-	reader.readAsText(event.target.files[0]); 
+	reader.readAsText(event.target.files[0]);
 }
 
